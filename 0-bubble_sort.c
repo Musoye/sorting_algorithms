a3b6b4db0877cf38419bc73f490068cff4389b5d
musoye <oyebamijimustapha44@gmail.com>
2023-01-18 07:44:42 -0800

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+void swap(int *array, int before, int after, int size);
+
+/**
+ * bubble_sort - an algorithm that implements bubble sort
+ * @array: the array
+ * @size: size of the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int count;
+	unsigned int i, a;
+
+	if (size < 2)
+		return;
+	count = -1;
+	for (a = 0; a < size - 1; a++)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(array, i, i + 1, size);
+				count++;
+			}
+		}
+		if (count < 1)
+		{
+			break;
+		}
+		count = 0;
+	}
+}
+/**
+ * swap - swap in two placees
+ * @array: the array
+ * @before: the first index
+ * @after: the second index
+ * Return: nothing
+ */
+void swap(int *array, int before, int after, int size)
+{
+	int value;
+
+	value = array[after];
+	array[after] = array[before];
+	array[before] = value;
+	print_array(array, size);
+}
--- sort.h
+++ sort.h
@@ -1,7 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,4 +14,7 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size); 
 #endif /** SORT_H */
