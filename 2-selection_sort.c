676f5bbb2a69e604764314da994d774b81d2f810
musoye <oyebamijimustapha44@gmail.com>
2023-01-19 02:38:08 -0800

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/*
+ * selection_sort - selection algorithm sort
+ * @array: the array
+ * @size: the size of the array
+ * Return: Nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min, holder;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			holder = array[i];
+			array[i] = array[min];
+			array[min] = holder;
+			print_array(array, size);
+		}
+	}
+}
